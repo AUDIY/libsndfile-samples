af60ad7755535023fd1d5166f7ec3da62d3b5014
AUDIY <96096729+AUDIY@users.noreply.github.com>
2025-01-16 21:58:20 +0900

    Add sample02_formatcheck.c
    To check the operation of sf_format_check() function.

---
+++ src/sample02_formatcheck.c
@@ -0,0 +1,56 @@
+/* sample02_formatcheck.c */
+
+/*
+** Copyright (C) 2025 AUDIY.
+**
+** All rights reserved.
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are
+** met:
+**
+**     * Redistributions of source code must retain the above copyright
+**       notice, this list of conditions and the following disclaimer.
+**     * Redistributions in binary form must reproduce the above copyright
+**       notice, this list of conditions and the following disclaimer in
+**       the documentation and/or other materials provided with the
+**       distribution.
+**     * Neither the author nor the names of any contributors may be used
+**       to endorse or promote products derived from this software without
+**       specific prior written permission.
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+** TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+** PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+** CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+** EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+** PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+** OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+** WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+** OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+** ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <sndfile.h>
+
+int main(void) {
+
+    const SF_INFO sfinfo0 = {0, 0, 0, 0, 0, 0}; // All 0
+    const SF_INFO sfinfo1 = {220501, 44100, 2, 0x10002, 1, 1}; // sfinfo of "TestSound_44100_16.wav"
+
+    printf("sf_format_check() returns TRUE(1) if the parameters are valid and FALSE(0) otherwise.\n\n");
+
+    /*
+        sf_format_check() allows the caller to check if a set of parameters in the SF_INFO struct
+        is valid before calling sf_open (SFM_WRITE).
+
+        sf_format_check() returns TRUE if the parameters are valid and FALSE otherwise.
+    */
+    printf("sfinfo0 sf_format_check: %d\n", sf_format_check(&sfinfo0)); // 0: FALSE (Invalid format)
+    printf("sfinfo1 sf_format_check: %d\n", sf_format_check(&sfinfo1)); // 1: TRUE (alid format)
+
+    return 0;
+}
